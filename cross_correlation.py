544765822761306493b710e32ee07604e20bd235
Rahul2013396 <42237507+Rahul2013396@users.noreply.github.com>
2022-02-18 16:46:27 +0530

    Add files via upload

--- cross_correlation.py
+++ cross_correlation.py
@@ -5,23 +5,38 @@ import sys
 from matplotlib import pyplot as plt
 from scipy import interpolate
 
+try:
+    wave , flux = np.loadtxt('Part1-reproducing_the_paper/piratbay_test/Giacobbe-main/HCN_Spectrum.dat', unpack=True)
+except:
+    wave , flux = np.loadtxt('/home/ws3/Desktop/rahul/pyrat_bay/Giacobbe/HCN_Spectrum.dat', unpack=True)
 
-wave , flux = np.loadtxt('Part1-reproducing_the_paper/piratbay_test/Giacobbe-main/HCN_Spectrum.dat', unpack=True)
-
-sys.path.append(os.path.abspath('/home/rahul/Desktop/notes/books_and_notes/liton_project/Part1-reproducing_the_paper/piratbay_test/Giacobbe-main'))
+try:
+    sys.path.append(os.path.abspath('/home/rahul/Desktop/notes/books_and_notes/liton_project/Part1-reproducing_the_paper/piratbay_test/Giacobbe-main'))
+except:
+    sys.path.append(os.path.abspath('/home/ws3/Desktop/rahul/pyrat_bay/Giacobbe'))
 
 import wavelength_Grid_modelling as gridding
 
-allspectrum = np.load('Part1-reproducing_the_paper/piratbay_test/Giacobbe-main/allspectrum.npy')
-wavelength = np.load('Part1-reproducing_the_paper/piratbay_test/Giacobbe-main/wavelength.npy')
+try:
+    allspectrum = np.load('Part1-reproducing_the_paper/piratbay_test/Giacobbe-main/allspectrum.npy')
+    wavelength = np.load('Part1-reproducing_the_paper/piratbay_test/Giacobbe-main/wavelength.npy')
+
+except:
+    allspectrum = np.load('/home/ws3/Desktop/rahul/pyrat_bay/Giacobbe/allspectrum.npy')
+    wavelength = np.load('/home/ws3/Desktop/rahul/pyrat_bay/Giacobbe/wavelength.npy')
+test_spectrum =[]
+
+
 wav1=[]
 flux1 =[]
 for i in range(len(wave)-1,-1,-1):
     wav1.append(wave[i])
     flux1.append(flux[i])
-print(wav1)
 
 f = interpolate.splrep(wav1,flux1)
+
+
+
 CCmatrixf = np.zeros((len(np.transpose(allspectrum[29])),167))
 for i in range(1):
     grid = gridding.grid_maker(wavelength[29]*1e-3)
@@ -39,7 +54,36 @@ for i in range(1):
     CCmatrixf += CCmatrix
 
 x = np.arange(1,168)
-plt.plot(x,CCmatrixf[21])
+
+plt.imshow(CCmatrixf, cmap='gray')
 plt.show()
+
+
+test =[]
+CCmatrixf = np.zeros((len(np.transpose(allspectrum[29])),167))
+for i in range(1):
+    grid = gridding.grid_maker(wavelength[29]*1e-3)
+    for i in range(64):
+        test_spectrum.append(interpolate.splev(grid[20+i],f))
+    plt.imshow(test_spectrum, cmap='gray')
+    plt.show()
+    CCmatrix = np.zeros((len(test_spectrum),len(grid)))
+
+    for i in range(len(CCmatrix)):
+        for j in range(len(CCmatrix[0])):
+            CCmatrix[i][j] = np.correlate(test_spectrum[i], interpolate.splev(grid[j],f))
+            
+
+    CCmatrixf += CCmatrix
+
+for i in range(len(CCmatrixf)):
+    a = max(CCmatrixf[i])
+    for j in range(len(CCmatrix[i])):
+        if(CCmatrixf[i][j] != a):
+            CCmatrixf[i][j] =0
+        else:
+            print(j)    
+
+
 plt.imshow(CCmatrixf, cmap='gray')
 plt.show()
